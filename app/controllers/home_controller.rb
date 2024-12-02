92bcf96c3dfb58d7e36d9e0acf035dbc52d03467
Nathan Trautenberg <Natester123@mac.com>
2024-12-01 21:06:08 -0700

    moved all website design mvc and routes over from other repo. Fixed an issue where text was overlapping when screen was shrink. Need to fix logo animation so it keeps repeating without turbo active.

---
+++ app/controllers/home_controller.rb
@@ -0,0 +1,24 @@
+class HomeController < ApplicationController
+  def index
+    @contact = Contact.new
+  end
+
+  def submit_contact
+    @contact = Contact.new(contact_params)
+
+    if @contact.save
+      flash[:notice] = "Your message has been submitted. Thank you!"
+      redirect_to root_path(anchor: 'contact')
+    else
+      flash[:alert] = "Please fill out all required fields."
+      redirect_to root_path(anchor: 'contact')
+    end
+  end
+
+  private
+
+  def contact_params
+    params.require(:contact).permit(:first_name, :last_name, :email, :message)
+  end
+
+end
